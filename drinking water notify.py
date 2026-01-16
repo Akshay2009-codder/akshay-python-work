e1af56bf9c1f1c2e58b7d6c3ee79a5dd8bc7cd01
Akshay dhumda <akshaydhumda@gmail.com>
2026-01-16 11:51:21 +0530

    Delete drinking water notify.py

--- drinking water notify.py
+++
@@ -1,9 +0,0 @@
-import time
-from plyer import notification
-
-if __name__ == "__main__":
-    notification.notify(
-        title="PLESE DRINK WATER",
-        message="bhaio or unki bahno pani pilo pani pani pina to jaruri hi hai",
-        timeout=10,
-    )
