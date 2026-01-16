f9e87fb19342c3a901cce9db1ee78c29da2c3448
Akshay dhumda <akshaydhumda@gmail.com>
2026-01-16 11:52:02 +0530

    Delete super and overloading in class.py

--- super and overloading in class.py
+++
@@ -1,9 +0,0 @@
-class A:
-    var = ("i am local varible ")
-    def __init__(self):
-        class_var_a = "i am a class variablen in class A"  #thish is a instance varieble
-
-        # class sabse pahle instance varieble exxess karta hai or baad me local
-
-
-
