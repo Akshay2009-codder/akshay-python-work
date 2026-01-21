e361c5023dffb1dbcb36bb3bc613c8c86f2e9c11
Akshay <akshaydhumda@gmail.com>
2026-01-21 10:07:17 +0530

    pushed

---
+++ Matplotlib/Backery_Sales.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+week = ["Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday"]
+
+shell = [10,7,20,4,20]
+
+plt.plot(week,shell)
+plt.show()
